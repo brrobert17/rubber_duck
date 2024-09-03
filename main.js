def794c0172d9de1fa5c77824e17e7269b08d978
Róbert Bári <br.robert17@gmail.com>
2024-09-03 15:25:27 +0200

    v1

---
+++ main.js
@@ -0,0 +1,33 @@
+const myButton = document.querySelector('#my-button');
+const myList = document.querySelector('#my-list');
+const duckImg = document.querySelector('#duck-img');
+myButton.addEventListener('click', (e) => {
+
+})
+
+const tooltip = document.getElementById('tooltip');
+duckImg.addEventListener('mouseenter', function(event) {
+    tooltip.classList.add('visible');
+});
+duckImg.addEventListener('mousemove', function(event) {
+    tooltip.style.left = `${event.clientX + 10}px`; // Position relative to viewport
+    tooltip.style.top = `${event.clientY + 10}px`;  // Position relative to viewport
+});
+duckImg.addEventListener('mouseleave', function() {
+    tooltip.classList.remove('visible');
+});
+
+myButton.addEventListener('click', ()=> addTodo())
+const addTodo =()=> {
+    if(myInput.value) {
+        const listElement = document.createElement('li');
+        listElement.innerText = myInput.value
+        myList.append(listElement);
+        myInput.value = '';
+    }
+}
+myInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTodo();
+    }
+})
